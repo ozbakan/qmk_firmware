f5702ecf4a518ebd83a8bea63c068ac4d85129ef
Tolga A. Ozbakan <tolga@ozbakan.com>
2025-09-12 14:53:26 +0300

    Simplify keymap

--- keyboards/crkbd/keymaps/ozbakan/keymap.c
+++ keyboards/crkbd/keymaps/ozbakan/keymap.c
@@ -1,88 +1,48 @@
-/*
-Copyright 2019 @foostan
-Copyright 2020 Drashna Jaelre <@drashna>
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+// CRKBD keymap by @ozbakan - macOS optimized
 
 #include QMK_KEYBOARD_H
 
-// Custom keycodes
 enum custom_keycodes {
-    CTL_SPC = SAFE_RANGE, // Sends Control + Space with reliable timing (macOS IME toggle)
+    CTL_SPC = SAFE_RANGE, // Control + Space for macOS input method toggle
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case CTL_SPC:
             if (record->event.pressed) {
-                // Hold Ctrl slightly before tapping Space to ensure macOS toggles input source
                 register_code(KC_LCTL);
                 wait_ms(30);
                 tap_code(KC_SPC);
                 wait_ms(10);
                 unregister_code(KC_LCTL);
             }
-            return false; // We handled it
+            return false;
     }
     return true;
 }
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT_split_3x6_3(
-  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
-  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI,   MO(1), LT(2, KC_ESC),   LALT_T(KC_ENT),   KC_SPC, KC_RCTL
-                                      //`--------------------------'  `--------------------------'
+    // Layer 0: Base QWERTY, Volume and Brightness
+    [0] = LAYOUT_split_3x6_3_ex2(
+        KC_TAB,  KC_Q, KC_W, KC_E, KC_R, KC_T, KC_F15,              KC_VOLU, KC_Y, KC_U, KC_I, KC_O, KC_P,    KC_BSPC,
+        KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_F14,              KC_VOLD, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
+        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B,                               KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+                KC_LGUI, MO(1), LT(2, KC_ESC),                               LALT_T(KC_ENT), KC_SPC, KC_RCTL
+    ),
 
-  ),
+    // Layer 1: Numbers, Navigation, Zoom
+    [1] = LAYOUT_split_3x6_3_ex2(
+        KC_LGUI,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_END,              KC_PGUP, KC_6,    KC_7,    KC_8,  KC_9,    KC_0,      KC_RGUI,
+        KC_LCTL, _______, _______, _______, _______, _______, KC_HOME,             KC_PGDN, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _______,   KC_RCTL,
+        KC_LSFT, _______, _______, _______, _______, _______,                               _______, _______, _______, _______, _______, KC_RSFT,
+                                   _______, _______, _______,                               LGUI(KC_PLUS), LGUI(KC_MINS), LGUI(KC_EQL)
+    ),
 
-    [1] = LAYOUT_split_3x6_3(
-  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TAB,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSPC,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______,                        KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, _______, _______,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, _______, CTL_SPC, KC_F14, KC_F15, _______,                      _______, _______, _______, _______, _______, KC_RSFT,
-  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          _______, _______, _______,     _______, _______, _______
-                                      //`--------------------------'  `--------------------------'
-  ),
-
-    [2] = LAYOUT_split_3x6_3(
-  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TAB, KC_EXLM, KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_LPRN, KC_RPRN, KC_ASTR, KC_BSPC,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL, LGUI(KC_1), LGUI(KC_2), LGUI(KC_3), LGUI(KC_4), KC_TILD,        KC_MINS,  KC_PLUS, KC_LBRC, KC_RBRC, KC_BSLS, _______,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, LGUI(KC_0), LGUI(KC_P0), LGUI(KC_MINS), LGUI(KC_PLUS), KC_GRV,  KC_UNDS,  KC_EQL, KC_LCBR, KC_RCBR, KC_PIPE, KC_RSFT,
-  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          _______, _______, _______,     _______, _______, _______
-                                      //`--------------------------'  `--------------------------'
-  )
+    // Layer 2: Symbols, VS Code shortcuts, Zoom
+    [2] = LAYOUT_split_3x6_3_ex2(
+        KC_LGUI,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR, KC_PERC, _______,              _______, KC_CIRC, KC_AMPR, KC_LPRN, KC_RPRN, KC_ASTR, KC_RGUI,
+        KC_LCTL, _______, _______, _______, _______, KC_TILD, _______,              _______, KC_MINS, KC_PLUS, KC_LBRC, KC_RBRC, KC_BSLS, KC_RCTL,
+        KC_LSFT, _______, _______, _______, _______, KC_GRV,                                 KC_UNDS, KC_EQL,  KC_LCBR, KC_RCBR, KC_PIPE, KC_RSFT,
+                                   _______, _______, _______,                                _______, _______, _______
+    )
 };
-
-#ifdef ENCODER_MAP_ENABLE
-const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-  [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_MPRV, KC_MNXT), ENCODER_CCW_CW(RM_VALD, RM_VALU), ENCODER_CCW_CW(KC_RGHT, KC_LEFT), },
-  [1] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_MPRV, KC_MNXT), ENCODER_CCW_CW(RM_VALD, RM_VALU), ENCODER_CCW_CW(KC_RGHT, KC_LEFT), },
-  [2] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_MPRV, KC_MNXT), ENCODER_CCW_CW(RM_VALD, RM_VALU), ENCODER_CCW_CW(KC_RGHT, KC_LEFT), },
-  [3] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_MPRV, KC_MNXT), ENCODER_CCW_CW(RM_VALD, RM_VALU), ENCODER_CCW_CW(KC_RGHT, KC_LEFT), },
-};
-#endif
